145c368205f00563952dd3d1597182d0d58f3966
sunfperi <sunf.peridot.9208@gmail.com>
2025-08-14 08:43:56 +0900

    Row、Colの削除

--- app/frontend/src/app/todo/page.tsx
+++ app/frontend/src/app/todo/page.tsx
@@ -10,17 +10,9 @@ import TodoListContainer from './TodoListKanban';
 export default function TodoPage() {
   return (
     <Container fluid="xxl" className="my-3">
-      {/*
-      <Row className="justify-content-md-center">
-        <Col md={12}>
-        */}
           {/* 静的なコンテンツはSSRされる */}
           {/* TodoListContainerはクライアント側でデータを取得するため、Suspenseは不要です */}
           <TodoListContainer />
-         { /*
-        </Col>
-      </Row>
-      */}
     </Container>
   );
 }
